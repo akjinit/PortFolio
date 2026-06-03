d07a5924c47aa69e26fed15e807a58a71ef2e5f9
akshat <akshatjha5787@gmail.com>
2026-06-03 16:06:24 +0530

    fix dark mode issue

--- react/src/components/ThemeToggle.jsx
+++ react/src/components/ThemeToggle.jsx
@@ -7,12 +7,12 @@ export const ThemeToggle = () => {
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      localStorage.setItem("theme", "light");
+    if (storedTheme === "light") {
       setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setIsDarkMode(true);
     }
   }, []);
 
